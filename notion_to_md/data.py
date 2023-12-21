9f41cadc3c938a3084c48ae56af807d902c23c0b
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-21 23:47:02 +0300

    feat(data): added docstrings

--- notion_to_md/data.py
+++ notion_to_md/data.py
@@ -4,6 +4,12 @@ from typing import Sequence
 
 @dataclass
 class FileInfo:
+    """
+    Attrs:
+        lines: Sequence[str]
+            Content of file with title inside.
+    """
+
     path_to_dir: str
     filepath: str
     heading: str
