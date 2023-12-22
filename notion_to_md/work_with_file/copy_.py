ab601173c8ad50d6cb74c9ee9e4fa5d4feb07dec
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-22 22:25:00 +0300

    fix(file:copy): changed `str` to `Path`

--- notion_to_md/work_with_file/copy_.py
+++ notion_to_md/work_with_file/copy_.py
@@ -4,10 +4,14 @@ from notion_to_md.data import FileInfo
 
 
 def copy_file_by_fileinfo(fileinfo: FileInfo) -> None:
-    fullpath = Path(fileinfo.path_to_dir) / Path(fileinfo.heading)
-    filepath = Path(f'{fullpath}.md')
+    if isinstance(fileinfo, FileInfo):
+        fullpath = fileinfo.path_to_dir / fileinfo.heading
+        filepath = Path(f'{fullpath}.md')
 
-    content = '\n'.join(fileinfo.lines[1:])
+        content = '\n'.join(fileinfo.lines[1:])
 
-    with Path.open(filepath, mode='w', encoding='utf-8') as file:
-        file.write(content)
+        with Path.open(filepath, mode='w', encoding='utf-8') as file:
+            file.write(content)
+    else:
+        exc_msg = 'Attribute `fileinfo` is not instance of FileInfo.'
+        raise TypeError(exc_msg)
