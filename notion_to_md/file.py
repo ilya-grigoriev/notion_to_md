af9ab6683157bcb28f56dc6bf7ac068e532a7bbc
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-22 22:25:42 +0300

    fix(file): removed line with `check_`

--- notion_to_md/file.py
+++ notion_to_md/file.py
@@ -1,8 +1,7 @@
-from notion_to_md.work_with_file import check_, copy_, get_, read_
+from notion_to_md.work_with_file import copy_, get_, read_
 
 
 class File:
     copy = staticmethod(copy_.copy_file_by_fileinfo)
-    check_exists = staticmethod(check_.check_file_exists)
     get_heading = staticmethod(get_.get_heading)
     read = staticmethod(read_.read_file)
