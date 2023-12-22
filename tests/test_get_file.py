000d9af48859385a0928fc554581f5c1a176db45
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-22 22:34:08 +0300

    fix(tests): added importing `pathlib`

--- tests/test_get_file.py
+++ tests/test_get_file.py
@@ -1,12 +1,14 @@
+from pathlib import Path
+
 from notion_to_md.data import FileInfo
 from notion_to_md.work_with_file.get_ import get_heading
 
 
-def test_get_heading_correct():
-    fileinfo = FileInfo('', '', '', ['# Test'])
+def test_get_heading_correct() -> None:
+    fileinfo = FileInfo(Path(), Path(), '', ['# Test'])
     assert get_heading(fileinfo) == 'Test'
 
 
-def test_get_file_heading_incorrect():
-    fileinfo = FileInfo('', '', '', [''])
-    get_heading(fileinfo)
+def test_get_file_heading_incorrect() -> None:
+    fileinfo = FileInfo(Path(), Path(), '', [''])
+    get_heading(fileinfo)   # Loguru output that there is error
--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from notion_to_md.work_with_file.read_ import read_file
@@ -5,16 +7,16 @@ from notion_to_md.work_with_file.read_ import read_file
 
 @pytest.mark.parametrize(
     'path',
-    ['tests/test_folder/test_file.md'],
+    [Path('tests/test_folder/test_file.md')],
 )
-def test_read_file_correct(path):
+def test_read_file_correct(path: Path) -> None:
     assert tuple(read_file(path)) == ('# Test', 'hello')
 
 
 @pytest.mark.parametrize(
     'path',
-    ['file_doesnt_exist'],
+    [Path('file_doesnt_exist')],
 )
-def test_read_file_incorrect(path):
+def test_read_file_incorrect(path: Path) -> None:
     with pytest.raises(FileNotFoundError):
         read_file(path)
