f048ef9bcc7a30ec05d82e6aef7baa6b9dc62521
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-22 22:25:08 +0300

    fix(file:read): changed `str` to `Path`

--- notion_to_md/work_with_file/read_.py
+++ notion_to_md/work_with_file/read_.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from typing import Any, Generator
 
-from notion_to_md.work_with_file.check_ import check_file_exists
 
-
-def read_file(filepath: str) -> Generator[str, Any, None]:
+def read_file(filepath: Path) -> Generator[str, Any, None]:
     """Read file by filepath.
 
     Parameters
@@ -17,8 +15,8 @@ def read_file(filepath: str) -> Generator[str, Any, None]:
     Generator[str, Any, None]
         Generator with lines of file.
     """
-    if check_file_exists(filepath):
-        with Path.open(Path(filepath), encoding='utf-8') as file:
+    if filepath.exists():
+        with Path.open(filepath, encoding='utf-8') as file:
             return (line.strip() for line in file.readlines())
     else:
         exc_msg = f"File '{filepath}' doesn't exist."
